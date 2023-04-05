d44d8550010f13903b43a50b37b04fa670a7bd5f
Manmohan <94291585+Manmohan-cripto@users.noreply.github.com>
2023-04-05 14:28:50 +0000

    Update blobConn.py
    remove string

--- blobConn.py
+++ blobConn.py
@@ -11,7 +11,7 @@ def main():
         df = pd.read_csv(url)
 
         #Set connection str for blob storage
-        connect_str = 'DefaultEndpointsProtocol=https;AccountName=containeradf;AccountKey=CR7/tnqSnpIDa9r2YYjn3DhN+L5QiTq1sz6GZ         Xs4JeCn/Snwra+oyAjjWLOUPfOzNKKf96HKQaWR+ASthTX+AQ==;EndpointSuffix=core.windows.net'
+        connect_str = '<string name>'
         my_container='output'
         time_now = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
         file_name =  "data_"+time_now +".csv"
